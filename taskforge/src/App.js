0eedde2e03f628d1240da5ce38a682470d99e72f
Your Name <arianna.mireles02@gmail.com>
2024-03-24 19:37:27 -0500

    Add css to login page

--- taskforge/src/App.js
+++ taskforge/src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
+import login from './loginPage.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src={login} className="App-login" alt="login" />
+      </header>
+      <footer>
+      <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,7 +16,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
